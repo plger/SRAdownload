d7d42290085494c381c92d91d88ae7dc4d1501b5
Pierre-Luc <pl.germain@gmail.com>
2021-11-28 10:39:55 +0100

    Create downloadFromSraRunTable.sh

---
+++ downloadFromSraRunTable.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+runtable=${1:-SraRunTable.txt}
+outdir=${2:-.}
+
+if [[ ! -r "$runtable" ]]; then
+  echo "Could not read run table."
+  echo "Usage: `basename $0` SraRunTable.txt [output_directory]"
+  echo "This downloads the SRA runs specified in the SraRunTable.txt."
+  echo "To get such a table, use the SRA run selector:"
+  echo "https://www.ncbi.nlm.nih.gov/Traces/study/"
+  echo "(Requires awk, pigz, curl and fasterq-dump)"
+  exit
+fi
+
+if [ ! -w "/path/to/dir" ]; then
+  echo "Target output directory ($outdir) is not writable!"
+  exit
+fi
+
+# we first group SRR runs by experiment:
+tmpfile=$outdir/$(mktemp runsByExp.XXXXXX)
+awk -vFPAT='([^,]*)|("[^"]+")' -vOFS=, '
+NR==1 {
+    expi = 1
+    geo = ""
+    for (i=1; i<=NF; i++){
+        if($i=="Experiment") expi=i
+        if($i=="GEO_Accession (exp)") geo=i
+    }
+    next
+};
+{
+    if(geo!=""){
+        print $expi"."$geo"\t"$1
+    }else{
+        print $expi"\t"$1
+    }
+}' | sort | awk '
+$1==x{
+    printf ",%s", $2
+    next
+}
+{
+    if(NR>1) printf "\n"
+    printf $1"\t"$2
+    x=$1
+}
+END {
+    printf "\n"
+}' $runtable > $tmpfile
+
+# we next proceed to download them
+while read -u 9 -r name runs
+do
+  # for each SRX/GSM ID
+  # first check if a folder already exists, if so skip
+  if [[ -d "$name" ]] ; then
+    echo "$name exists, skipping..."
+  else
+    echo $name
+    mkdir $name
+    for SRR in `echo $runs | tr "," "\n"`; do
+      bash `dirname $0`/downloadOneRun.sh "$SRR" "$outdir/$name"
+    done
+  fi
+done 9< $tmpfile
+
+rm $tmpfile
+
+echo "Done!"
